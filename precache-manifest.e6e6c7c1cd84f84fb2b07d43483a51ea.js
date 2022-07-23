a25e9814cf6f42ba37884ad88138c66d243cd59d
RAHUL KUMAR <rahulkumar4871@gmail.com>
2022-07-23 17:20:33 +0530

    Updates

---
+++ precache-manifest.e6e6c7c1cd84f84fb2b07d43483a51ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05000522dc389400e3d3",
+    "url": "/TextUtils-React/static/css/main.622957eb.chunk.css"
+  },
+  {
+    "revision": "05000522dc389400e3d3",
+    "url": "/TextUtils-React/static/js/main.05000522.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "cb6922165b6ed48946a35bed8fd785fe",
+    "url": "/TextUtils-React/index.html"
+  }
+];
